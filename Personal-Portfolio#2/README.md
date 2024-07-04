2eff017236e8f00f7cbee49cbb32a313acf625b1
luis-afonso136 <luis.manuel.afonso@ipvc.pt>
2024-07-04 16:41:58 +0100

    Personal Portfolio

---
+++ Personal-Portfolio#2/README.md
@@ -0,0 +1,9 @@
+# Personal-Portfolio #2
+
+This is a simple personal portfolio made in HTML, CSS, JS and Bootstrap
+
+# Screenshot
+
+Here we have project screenshot :
+
+![screenshot-1](Screenshot.png)
---
+++ Personal-Portfolio#2/script.js
@@ -0,0 +1,14 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+    });
+}
+
+window.addEventListener('scroll', function () {
+    var scrollTopButton = document.querySelector('.scroll-top');
+    if (this.window.pageYOffset > 200) {
+        scrollTopButton.style.display = 'block';
+    } else {
+        scrollTopButton.style.display = 'none';
+    }
+});
